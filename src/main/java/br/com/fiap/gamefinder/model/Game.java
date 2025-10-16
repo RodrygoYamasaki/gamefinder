890db95cfb2f108ae75289396489660c91d6351a
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-10-16 09:25:52 -0300

    Fixing the errors

--- src/main/java/br/com/fiap/gamefinder/model/Game.java
+++ src/main/java/br/com/fiap/gamefinder/model/Game.java
@@ -2,10 +2,19 @@ package br.com.fiap.gamefinder.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Game {
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,8 +28,10 @@ public class Game {
 
     private Double rating;
 
+    @ManyToOne
     private Genre genre;
 
+    @ManyToOne
     private Platform platform;
 
     private String coverUrl;
